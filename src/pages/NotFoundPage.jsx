0303e2e32d26561b2b1f811f1edcd1b2b63bbf9f
Sufianing <139410353+Sufianing@users.noreply.github.com>
2026-02-12 16:10:52 +0800

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 function NotFoundPage() { 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page is Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </div>
   );
